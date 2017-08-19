692c28943ef3841b1724efea7f3787970f4db6f1
jaketrent <trent.jake@gmail.com>
2017-08-19 16:20:02 -0600

    chore: remove console logs and comments

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -3,15 +3,7 @@ module.exports = (config, env) => {
   const babelRule = config.module.rules[1].oneOf[1]
 
   eslintRule.options.useEslintrc = true
-
-  // set babelrc true
-  // use export enhancements
-  // "glamorous-displayname"
-
   babelRule.options.babelrc = true
-
-  console.log('eslint', eslintRule)
-  console.log('babel', babelRule)
 
   return config
 }
