9c41750e4c7da6de03008980697cd9c7e549ef39
jaketrent <trent.jake@gmail.com>
2017-08-19 18:16:54 -0600

    refactor(client): remove todo on table ui

--- client/src/table/index.js
+++ client/src/table/index.js
@@ -72,13 +72,4 @@ export default _ =>
       </Card>
     </Cards>
     <CardsPlayed turn={turn} />
-    <ul>
-      <li>show cards</li>
-      <li>draw cards on turn</li>
-      <li>buy cards on turn</li>
-      <li>active player</li>
-      <li>remaining actions</li>
-      <li>remaining buys</li>
-      <li>played cards on turn</li>
-    </ul>
   </div>
