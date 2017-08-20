b737fdadaa4221c6ead13334f05350e6274577a4
jaketrent <trent.jake@gmail.com>
2017-08-20 08:23:05 -0600

    feat(client): fetch games from server

--- client/src/list/index.js
+++ client/src/list/index.js
@@ -1,12 +1,38 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-export default _ =>
-  <div>
-    <h2>games list</h2>
-    <ul>
-      <li>
-        <Link to="/game/abc123">abc123</Link>
-      </li>
-    </ul>
-  </div>
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { fetched: false, games: [] }
+  }
+  async componentDidMount() {
+    const res = await fetch(`${process.env.REACT_APP_API_HOST}/api/games`)
+    const json = await res.json()
+    this.setState(_ => ({ fetched: true, games: json.data }))
+  }
+  renderLoading() {
+    return <div>Loading...</div>
+  }
+  renderGames() {
+    return (
+      <ul>
+        <li>
+          {this.state.games.map(game =>
+            <Link to={`/game/${game.id}`}>
+              {game.id}
+            </Link>
+          )}
+        </li>
+      </ul>
+    )
+  }
+  render() {
+    return (
+      <div>
+        <h2>games list</h2>
+        {this.state.fetched ? this.renderGames() : this.renderLoading()}
+      </div>
+    )
+  }
+}
